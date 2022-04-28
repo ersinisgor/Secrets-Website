fea1b63596078bf07aed0b7643ae802f025fd3b9
ersin <ersin@email.com>
2022-04-28 12:56:47 +0300

    Add salting and hashing password

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 
 const app = express();
@@ -41,31 +42,40 @@ app.get("/register", function(req, res){
 
 
 app.post("/register", function(req,res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function(err){
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        }); 
     });
 
-    newUser.save(function(err){
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    }); 
+    
 });
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.username);
+    const password = req.body.username;
 
     User.findOne({email: username}, function(err, founduser){
         if (err) {
             console.log(err);
         } else {
             if (founduser) {
-                if (founduser.password === password) {
-                    res.render("secrets");
+                bcrypt.compare(password, founduser.password, function(err, result) {
+                    if (result === true){
+                        res.render("secrets");
+                    }
+                });
+                    
                 }
             }
         }
